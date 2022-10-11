c9176d39b97089d48a70b58537f4dfe7b649e3ae
annakiseliova <annakiseliova7@gmail.com>
2022-10-11 13:39:45 +0300

    Массивы 2. Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //Массивы 2. Задание 1
         System.out.println("Массивы 2. Задание 1");
-        int[] weights = {90, 91, 92, 85, 87, 88, 89, 0, 0, 0, 0};
-        int[] weightsCopy = {902, 91, 92, 85, 87, 88, 89, 0, 0, 0, 0};
-        boolean arraysAreEqual = true;
-        arraysAreEqual = weights.length == weightsCopy.length;
-        if (arraysAreEqual) {
-            for (int i = 0; i < weights.length; i++) {
-                if (weights[i] != weightsCopy[i]) {
-                    arraysAreEqual = false;
-                }
-            }
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (100_000 + (Math.random() * (200_000 - 100_000)));
         }
-        if (arraysAreEqual) {
-            System.out.println("Массивы одинаковые");
-        } else {
-            System.out.println("Массивы разные");
+        String arrString = Arrays.toString(arr);
+        System.out.println(arrString);
+        int sumAllMonth = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumAllMonth = sumAllMonth+ arr[i];
         }
-        int maxWeight = -1;
-        for (final int current : weights) {
-            if (current > maxWeight) {
-                maxWeight = current;
-            }
-        }
-        System.out.println(maxWeight);
-        for (int i = 0; i < weights.length - 1 && weights[i + 1]!=0 ; i++) {
-            System.out.println(weights[i+1] -weights[i]);
-        }
+        System.out.println("Сумма трат за месяц составила " + sumAllMonth + " рублей");
 
     }
 }
+
