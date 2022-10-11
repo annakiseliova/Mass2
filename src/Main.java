85ce2de1254d9f29e325e0e64866f67f8d01e5c3
annakiseliova <annakiseliova7@gmail.com>
2022-10-11 13:49:29 +0300

    Массивы 2. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,26 @@ public class Main {
         System.out.println(arrString);
         int sumAllMonth = 0;
         for (int i = 0; i < arr.length; i++) {
-            sumAllMonth = sumAllMonth+ arr[i];
+            sumAllMonth = sumAllMonth + arr[i];
         }
         System.out.println("Сумма трат за месяц составила " + sumAllMonth + " рублей");
+
+        //Массивы 2. Задание 2
+        System.out.println("Массивы 2. Задание 2");
+
+        int minSum = 200_000;
+        int maxSum = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxSum) {
+                maxSum = arr[i];
+            }
+            if (arr[i]<minSum){
+                minSum = arr[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за день составила " + maxSum + " рублей");
+        System.out.println("Минимальная сумма трат за день составила " + minSum + " рублей");
+
 
     }
 }
