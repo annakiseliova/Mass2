3d1478e34bd8dcbb6d3f8e0fe98fc1a2d0e1a5b2
annakiseliova <annakiseliova7@gmail.com>
2022-10-11 13:57:50 +0300

    Массивы 2. Задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,22 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //Массивы 2. Задание 1
-        System.out.println("Массивы 2. Задание 1");
+        //Массивы 2. Задание 3
+        System.out.println("Массивы 2. Задание 3");
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (100_000 + (Math.random() * (200_000 - 100_000)));
         }
         String arrString = Arrays.toString(arr);
         System.out.println(arrString);
-        int sumAllMonth = 0;
+
+        double averageOfMonths = 0;
+        int sumMonth = 0;
         for (int i = 0; i < arr.length; i++) {
-            sumAllMonth = sumAllMonth + arr[i];
+            sumMonth = sumMonth +arr[i];
+            averageOfMonths = sumMonth/arr.length;
         }
-        System.out.println("Сумма трат за месяц составила " + sumAllMonth + " рублей");
-
-        //Массивы 2. Задание 2
-        System.out.println("Массивы 2. Задание 2");
-
-        int minSum = 200_000;
-        int maxSum = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > maxSum) {
-                maxSum = arr[i];
-            }
-            if (arr[i]<minSum){
-                minSum = arr[i];
-            }
-        }
-        System.out.println("Максимальная сумма трат за день составила " + maxSum + " рублей");
-        System.out.println("Минимальная сумма трат за день составила " + minSum + " рублей");
-
-
+        System.out.println("Средняя сумма трат за месяц составила " + averageOfMonths +" рублей");
     }
 }
 
